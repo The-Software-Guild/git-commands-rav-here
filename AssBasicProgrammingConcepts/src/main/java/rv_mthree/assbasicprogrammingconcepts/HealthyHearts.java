534d7dda40b5096c06ee2908fb35f83f5b1ea6b7
Rachel Vickerman <rachvick21@hotmail.co.uk>
2022-10-13 23:24:10 +0100

    Assessment: Basic Programming Concepts

--- AssBasicProgrammingConcepts/src/main/java/rv_mthree/assbasicprogrammingconcepts/HealthyHearts.java
+++ AssBasicProgrammingConcepts/src/main/java/rv_mthree/assbasicprogrammingconcepts/HealthyHearts.java
@@ -13,21 +13,25 @@ import java.util.Scanner;
 public class HealthyHearts {
     
     public static void main(String[] args) {
-        
+        // initialise variables for age
         String sage;
-        
         int age;
         
+        // initialise scanner
         Scanner inputReader = new Scanner(System.in);
+        // print question
         System.out.println("What is your age?");
+        // record user input
         sage = inputReader.nextLine();
+        // change input from string to integer
         age = Integer.parseInt(sage);
         
-        
+        // find max heart rate and target zone values 
         int maxHR = 220 - age;
         int l = (maxHR/100)*50;
         int h = (maxHR/100)*85;
         
+        // print results
         System.out.println("Your Maximum heart rate should be " + maxHR + " beats per minute.");
         System.out.println("Your target HR zone is " + l + "-" + h + " beats per minute" 
         );
