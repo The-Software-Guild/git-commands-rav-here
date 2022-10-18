fa1c418ef9e4a8ff6e184b377199a6a1ee3d317a
Rachel Vickerman <rachvick21@hotmail.co.uk>
2022-10-18 23:58:58 +0100

    Finished fruit salad ex

--- PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/arrays/FruitSalad.java
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/arrays/FruitSalad.java
@@ -4,7 +4,8 @@
  */
 package com.sg.foundations.flowcontrol.arrays;
 
-import java.util.Arrays;
+//import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,39 +16,50 @@ public class FruitSalad {
     public static void main(String[] args) {
         
         String[] fruit = {"Kiwi Fruit", "Gala Apple", "Granny Smith Apple", "Cherry Tomato", "Gooseberry", "Beefsteak Tomato", "Braeburn Apple", "Blueberry", "Strawberry", "Navel Orange", "Pink Pearl Apple",  "Raspberry", "Blood Orange", "Sungold Tomato", "Fuji Apple", "Blackberry", "Banana", "Pineapple", "Florida Orange", "Kiku Apple", "Mango", "Satsuma Orange", "Watermelon", "Snozzberry"};
-
-        String[] fruitSalad = new String[12];
+        ArrayList<String> fruitSalad = new ArrayList<>();
+        //String[] fruitSalad = new String[12];
         //System.out.println(Arrays.toString(fruitSalad));
         int apples = 0; 
         int oranges = 0;
+        int i = 0;
         
-        for (int i=0; i < fruitSalad.length; i++) {
-            
-            do { 
-                if (fruit[i].contains("berry")) {
-                    fruitSalad[i] = fruit[i];
+        while(fruitSalad.size() < 12) { 
+
+            if (fruit[i].contains("berry")) {
+                fruitSalad.add(fruit[i]);
+                i += 1;
+            }
+            else if (fruit[i].contains("Apple")) {
+                if (apples < 2) {
+                    fruitSalad.add(fruit[i]);
+                    apples += 1; 
+                    i += 1;
                 }
-                else if (fruit[i].contains("Apple")) {
-                    if (apples < 2) {
-                        fruitSalad[i] = fruit[i];
-                        apples += 1;  
-                    }  
+                else {
+                    i += 1;
                 }
-                else if (fruit[i].contains("Orange")) {
-                    if (oranges < 2) { 
-                        fruitSalad[i] = fruit[i];
-                        oranges += 1;
-                    }      
+            }
+            else if (fruit[i].contains("Orange")) {
+                if (oranges < 2) { 
+                    fruitSalad.add(fruit[i]);
+                    oranges += 1;
+                    i += 1;
                 }
-                else if (!fruit[i].contains("Tomato")) {
-                    fruitSalad[i] = fruit[i];
+                else {
+                    i += 1;
                 }
-                //System.out.println(Arrays.toString(fruitSalad));
-           
-            } while (fruitSalad.length < 12);
-        
-        }
-        System.out.println("My fruit salad contains " + Arrays.toString(fruitSalad));
+            }
+            else if (!fruit[i].contains("Tomato")) {
+                fruitSalad.add(fruit[i]);
+                i += 1;
+            }
+            else {
+                i += 1;
+            }
+
+        } 
+
+        System.out.println("My fruit salad contains " + fruitSalad);
     }
     
 }
